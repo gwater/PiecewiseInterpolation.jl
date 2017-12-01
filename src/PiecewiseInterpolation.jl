40ac4ca0ea93eeb2c9dc720ef240366699b75d12
Josua Grawitter <josh@greyage.org>
2017-12-01 15:16:01 -0500

    rewrite for more flexibility, consistency

--- src/PiecewiseInterpolation.jl
+++ src/PiecewiseInterpolation.jl
@@ -2,51 +2,68 @@ module PiecewiseInterpolation
 
 using Dierckx
 
-export update_interpolations!, interpolation
+import Base: append!
 
-function append_interpolation!(interpolations, times, values, tau)
-    n_last_tau = length(interpolations)
-    # searchsorted returns a range; its `stop` is the last index before `t`
-    begin_i  = searchsorted(times, n_last_tau * tau).start
-    end_i = searchsorted(times, (n_last_tau + 1) * tau).stop
-    index_diff = end_i - begin_i
-    if index_diff < 1
-        error("insufficient data")
-    end
-    order = min(5, index_diff)
-    interpolation = Spline1D(view(times, begin_i:end_i),
-                             view(values, begin_i:end_i), k = order)
-    push!(interpolations, interpolation)
+export PieceInterpolation, append!
+
+immutable PieceInterpolation{T <: Real}
+    pieces::Vector{Spline1D}
+    intervals::Vector{Tuple{T, T}}
 end
 
-function update_interpolations!(interpolations, times, values, tau)
-    n_tau = ceil(Int, times[end] / tau)
-    if length(interpolations) == n_tau
-        # remove the last interpolation, so we can update it
-        pop!(interpolations)
+function (p::PieceInterpolation)(t)
+    i = searchsortedlast(p.intervals, t, by=interval -> interval[1])
+    return p.pieces[i](t)
+end
+
+function get_time_intervals{T <: Real}(jumps::Vector{T},
+                                       t_start::T, t_end::T)
+    if length(jumps) == 0
+        return [(t_start, t_end)]
     end
-    while length(interpolations) < n_tau
-        try
-            append_interpolation!(interpolations, times, values, tau)
-        catch exc
-            if isa(exc, ErrorException)
-                warn(exc.msg)
-                break
-            else
-                rethrow(exc)
-            end
-        end
+    a = Vector{Tuple{T, T}}(length(jumps) + 1)
+    a[1] = (t_start, jumps[1])
+    for i in 2:length(jumps)
+        a[i] = (jumps[i - 1], jumps[i])
+    end
+    a[end] = (jumps[end], t_end)
+    return a
+end
+
+get_index_interval(times, t_start, t_end) =
+    searchsortedfirst(times, t_start):searchsortedlast(times, t_end)
+
+function append!{T}(p::PieceInterpolation{T}, new_times::Vector{T},
+                    new_values::Vector, new_jumps::Vector{T})
+    assert(length(new_times) == length(new_values))
+    old_t_end = p.intervals[end][2]
+    if any(new_times .< old_t_end) || any(new_jumps .< old_t_end)
+        error("new data overlaps with existing interpolations")
+    end
+    time_intervals = get_time_intervals(new_jumps, old_t_end, new_times[end])
+    for time_interval in time_intervals
+        indices = get_index_interval(new_times, time_interval...)
+        interpolation =
+            Spline1D(view(new_times, indices), view(new_values, indices))
+        push!(p.pieces, interpolation)
+        push!(p.intervals, time_interval)
     end
     return nothing
 end
 
-function interpolation{T <: Real}(t::T, interpolations, tau::T,
-                                  history::Function)
-    if t <= 0
-        return history(t)
-    end
-    piece_n = ceil(Int, t / tau)
-    return interpolations[piece_n](t)
+function PieceInterpolation{T <: Real}(times::Vector{T}, values::Vector,
+                                       jumps::Vector{T})
+    assert(length(times) == length(values))
+    # create object with first interval, then append remaining
+    time_interval = (times[1], jumps[1])
+    indices = 1:searchsortedlast(times, jumps[1])
+    interpolation =
+        Spline1D(view(times, indices), view(values, indices))
+    p = PieceInterpolation([interpolation], [time_interval])
+    rest_start = searchsortedfirst(times, jumps[1])
+    append!(p, times[rest_start:end], values[rest_start:end],
+            jumps[2:end])
+    return p
 end
 
 end #module
