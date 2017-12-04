5bacec24e4caa5739687e6a7299789cedf8936d4
Josua Grawitter <josh@greyage.org>
2017-12-04 14:47:11 -0500

    rewrite using IntervalSets

--- src/PiecewiseInterpolation.jl
+++ src/PiecewiseInterpolation.jl
@@ -1,71 +1,74 @@
 module PiecewiseInterpolation
 
+using IntervalSets
 using Dierckx
-
 import Base: append!
 
-export PieceInterpolation, append!
+export PiecewiseSpline1D, append!
 
-immutable PieceInterpolation{T <: Real}
+immutable PiecewiseSpline1D{T <: Real}
     pieces::Vector{Spline1D}
-    intervals::Vector{Tuple{T, T}}
+    intervals::Vector{ClosedInterval{T}}
 end
 
-function (p::PieceInterpolation)(t)
-    i = searchsortedlast(p.intervals, t, by=interval -> interval[1])
+function (p::PiecewiseSpline1D)(t)
+    i = findlast(interval -> t ∈ interval, p.intervals)
     return p.pieces[i](t)
 end
 
-function get_time_intervals{T <: Real}(jumps::Vector{T},
-                                       t_start::T, t_end::T)
-    if length(jumps) == 0
-        return [(t_start, t_end)]
-    end
-    a = Vector{Tuple{T, T}}(length(jumps) + 1)
-    a[1] = (t_start, jumps[1])
+function split_interval{T <: Real}(jumps::Vector{T},
+                                   interval::ClosedInterval{T})
+    isempty(jumps) && return [interval]
+    intervals = Vector{typeof(interval)}(length(jumps) + 1)
+    intervals[1] = interval.left..jumps[1]
     for i in 2:length(jumps)
-        a[i] = (jumps[i - 1], jumps[i])
+        intervals[i] = jumps[i - 1]..jumps[i]
     end
-    a[end] = (jumps[end], t_end)
-    return a
+    intervals[end] = jumps[end]..interval.right
+    return intervals
 end
 
-get_index_interval(times, t_start, t_end) =
-    searchsortedfirst(times, t_start):searchsortedlast(times, t_end)
+function get_index_range{T}(times::Vector{T}, interval::ClosedInterval{T})
+    f = t -> t ∈ interval
+    return findfirst(f, times):findlast(f, times)
+end
 
 get_interpolation(times, values) =
-    Spline1D(times, values, bc="extrapolate", k=min(5, length(times)))
+    Spline1D(times, values, bc="extrapolate", k = min(5, length(times)))
 
-function append!{T}(p::PieceInterpolation{T}, new_times::Vector{T},
+function append!{T}(p::PiecewiseSpline1D{T}, new_times::Vector{T},
                     new_values::Vector, new_jumps::Vector{T})
-    assert(length(new_times) == length(new_values))
-    old_t_end = p.intervals[end][2]
+    old_t_end = p.intervals[end].right
     if any(new_times .< old_t_end) || any(new_jumps .< old_t_end)
         error("new data overlaps with existing interpolations")
     end
-    time_intervals = get_time_intervals(new_jumps, old_t_end, new_times[end])
-    for time_interval in time_intervals
-        indices = get_index_interval(new_times, time_interval...)
+    intervals = split_interval(new_jumps, old_t_end..new_times[end])
+    for interval in intervals
+        indices = get_index_range(new_times, interval)
         interpolation = get_interpolation(view(new_times, indices),
                                           view(new_values, indices))
         push!(p.pieces, interpolation)
-        push!(p.intervals, time_interval)
+        push!(p.intervals, interval)
     end
     return nothing
 end
 
-function PieceInterpolation{T <: Real}(times::Vector{T}, values::Vector,
-                                       jumps::Vector{T})
+"""
+    PiecewiseSpline1D(times::Vector, values::Vector, jumps::Vector)
+
+Construct a spline interpolation (based on Dierckx) which takes into account first order discontinuties.
+"""
+function PiecewiseSpline1D{T <: Real}(times::Vector{T}, values::Vector,
+                                      jumps::Vector{T})
     assert(length(times) == length(values))
     # create object with first interval, then append remaining
-    time_interval = (times[1], jumps[1])
+    time_interval = times[1]..jumps[1]
     indices = 1:searchsortedlast(times, jumps[1])
     interpolation =
         get_interpolation(view(times, indices), view(values, indices))
-    p = PieceInterpolation([interpolation], [time_interval])
+    p = PiecewiseSpline1D([interpolation], [time_interval])
     rest_start = searchsortedfirst(times, jumps[1])
-    append!(p, times[rest_start:end], values[rest_start:end],
-            jumps[2:end])
+    append!(p, times[rest_start:end], values[rest_start:end], jumps[2:end])
     return p
 end
 
